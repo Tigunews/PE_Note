d569a645bae4495529536cf32708944d85405c96
Tigunews <gkr0325@hanmail.net>
2020-09-07 23:17:03 +0900

    Update note_ds2.js

--- note_ds2.js
+++ note_ds2.js
@@ -1010,7 +1010,7 @@ Mobility를 기반으로 오픈 API기반으로 다양한 서비스를 연계 �
 <img src = "./img/LOD.png" style = "max-width:100%; height:auto;"><br/>\
 - URI(Uniform Resource Identifier) : 인터넷에 존재하는 객체 표현 <br/>\
 - XML : 메타언어 <br/>\
-- RDF(Resource Descriptiono Framework) : 정보 자원, 구조 표현 언어 <br/>\
+- RDF(Resource Description Framework) : 정보 자원, 구조 표현 언어 <br/>\
 - RDFS : RDF의 Schma 정보 <br/>\
 - OWL : DAML + OIL, Web ontology 언어 <br/>\
 - SPARQL : 온톨로지 질의 언어 <br/>\
