63a4aeb5ee69eaf8f8e26edd8405a070c5daff8f
Tigunews <gkr0325@hanmail.net>
2020-10-10 14:24:40 +0900

    Update note_ds.js

--- note_ds.js
+++ note_ds.js
@@ -1429,7 +1429,7 @@ var answer = answer.concat(
 - Supervisiory Cotnrol And Data Acquisition <br/>\
 - 통신 경로상의 아날로그 또는 디지털 신호를 사용하여 원격장치의 상태정보 데이터를 RTU로 수집/수신/기록/표시하여 중앙 제어 시스템에 원격 장치를 감시 제어하는 시스템 <br/><br/>\
 # 구성요소 <br/>\
-- HMI : 기계 데이터 친숙 형태 변환 <br/>\
+- HMI(Human Machine Interface) : 사용자, 시스템간 통신 대시보드 <br/>\
 - PLC(Programmable Logic Controller) : 시퀀스 제어 + 수치 연산 제어장치 <br/>\
 - RTU(Remote Turminal Unit) : 공정에 설치된 센서와 직접 연결장치<br/><br/>\
 # SCADA DCS 비교 <br/>\
