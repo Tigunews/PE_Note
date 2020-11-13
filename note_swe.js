17c87901527cf282aaf54e5386711c6c316b5f95
Tigunews <gkr0325@hanmail.net>
2020-11-13 19:46:52 +0900

    Update note_swe.js

--- note_swe.js
+++ note_swe.js
@@ -970,8 +970,10 @@ var answer = answer.concat(
 ',
 
 // ISO/IEC/IEEE 42010
-'# 정의 : 소프트웨어 아키텍퍼 표준 정립 <br/>\
+'# 정의 : 소프트웨어 아키텍처 표준 정립 <br/>\
 - 소프트웨어 집약적 시스템의 아키텍처 기술 요소와 행위를 표준화하여 다양한 소프트웨어를 설계 및 활용할 수 있게 하고, 이해관계자들이 의사소통이 용이하도록 만든 표준 메타 모델 <br/><br/>\
+# 개념도 <br/>\
+<img src = "./img/ISOIECIEEE42010Model.png" style = "max-width:100%; height:auto;"><br/><br/>\
 # 채택 동향 <br/>\
 - 최초 표준 : IEEE Std 1471:2000 <br/>\
 - ISO 채택 : ISO/IEC 42010:2007 승인 <br/>\
