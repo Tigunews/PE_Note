fb824a943599aaeea0732f8ac4595069600e9622
Tigunews <gkr0325@hanmail.net>
2020-12-23 10:08:55 +0900

    Update note_ca.js

--- note_ca.js
+++ note_ca.js
@@ -1374,7 +1374,7 @@ FROM sys.dm_os_memory_clerks <br/><br/>\
 '# 정의 : 데이터의 가용성 및 성능 향상을 위한 디스크 중복구조 <br/>\
 - Redundant Array of Independent Disks <br/>\
 - 다수의 디스크에 데이터를 중복으로 결합하여 가용성과 성능을 향상시키는 저장장치 기술 <br/><br/>\
-# 암기 <br/>\
+# 암기 (스미비바단분이)<br/>\
 <img src = "./img/BasicRAID.png" style = "max-width:100%; height:auto;"><br/><br/>\
 # 상세 <br/>\
 <img src = "./img/RAID_Detail1.png" style = "max-width:100%; height:auto;"><br/>\
