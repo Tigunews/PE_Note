ea6aa0df2e75afb2a54c50f822651b61f249423d
Tigunews <gkr0325@hanmail.net>
2021-03-02 16:28:32 +0900

    Update note_sec.js

--- note_sec.js
+++ note_sec.js
@@ -62,6 +62,8 @@ var question = question.concat(
 '클라우드 컴퓨팅 보안',
 'CVE',
 '암호화 기술',
+'Hash Function',
+'[Hash Function]- Hash Algorithm',
 '[암호화 기술]- 양자암호통신',
 '[양자 보안]- QKD',
 '[양자 보안]- PQC',
@@ -1183,6 +1185,35 @@ var answer = answer.concat(
 <img src = "./img/암호화기술_1.png" style = "max-width:100%; height:auto;"><br/><br/>\
 <img src = "./img/암호화기술_2.png" style = "max-width:100%; height:auto;"><br/><br/>\
 <img src = "./img/암호화기술_3.png" style = "max-width:100%; height:auto;">\
+',
+  
+// Hash Function
+'# 개념 : 수학식 구성, 임의 길이 -> 고정길이 출력 함수 <br/>\
+- 임의의 길이의 데이터를 "해시 알고리즘을 이용하여" 고정된 길이 데이터로 매핑하는 함수 <br/>\
+- Input : Hash Key / Output : Bucket home address <br/><br/>\
+# 특징 <br/>\
+- 고정된 길이의 값 출력 <br/>\
+- 입력 값 일부 변경 되더라도 전혀다른 결과값 출력 (Snow ball) <br/>\
+- 출력값 토대로 입력값 유추 불가능(역상 저항성) <br/>\
+- 입력값은 항상 동일한 해시값 출력 <br/><br/>\
+# 종류 <br/>\
+- 암호학적 : MD5, SHA 계열 <br/>\
+- 비 암호학적 : CRC32 <br/><br/>\
+# SHA 알고리즘 종류 <br/>\
+<img src = "./img/SHA.png" style = "max-width:100%; hegiht:auto;"><br/><br/>\
+* 120회 관리 1교시 9번 <br/>\
+* 116회 관리 1교시 3번\
+',
+
+// Hash Algorithm
+'# 정의 : Hash function + Encryption <br/>\
+- 임의의 길이의 메시지를 일정 고정길이의 해쉬 값으로 변환시켜주는 단방향성 알고리즘 <br/><br/>\
+# 조건 <br/>\
+<img src = "./img/HashAlgorithmCondition.png" style = "max-width:100%; hegiht:auto;"><br/><br/>\
+# 원리 <br/>\
+<img src = "./img/HashAlgorithMechanism.png" style = "max-width:100%; hegiht:auto;"><br/><br/>\
+* 120회 관리 1교시 9번 <br/>\
+* 116회 1교시 3번\
 ',
    
 // [암호화기술]- 양자암호통신
