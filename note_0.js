4e8ba012891ef0b98b3b30170133696532cf290b
Tigunews <gkr0325@hanmail.net>
2021-05-01 20:37:00 +0900

    202105012036

--- note_0.js
+++ note_0.js
@@ -8,39 +8,40 @@ var this_week_question =
 [
 '',
 
-/* 9주차 */
+// 9주차 
 '가설검정','레그테크','섭테크','프롭테크','트랜잭션','Random Sampling','Filtering','K-Means','SOM','Isolation Level','선형회귀모형 추론 가정','Smart Factory','위험관리','IT-Compliance','COBIT 5.0','그로스 해킹','정규 표현식','유한 오토마타','가설검정','병행제어','SVM','ISO 19626','Hilbert',
-
-/* 8주차 */
+/*
+// 8주차 
 'Library','Python','ALM','SW Visualization','ARM','정보시스템 하드웨어 규모산정 지침','CMMI','ISO/IEC 33000','일정관리','WBS','Thread','Process','MVC','MVP','MVVM','테스트 자동화','DB 복제','Scheduling','비용산정','분기 예측',
 
-/* 7주차 */
+// 7주차 
 'CDR','디지털 포렌식','Secure Coding','무자각 지속 인증','NFV','SDN','Pre-Crime','대역확산','Lateral Movement','양자암호통신','바이오정보 분산관리','FIDO','APT','DDoS','인증기술',
 
-/* 6주차 */
+// 6주차 
 '지능형 CCTV','LBS','RTLS','Lidar','클라우드','의료기술','컨테이너 오케스트레이션','SCADA','Smart City 보안','인공지능 서비스 플랫폼','CPS','디지털 트윈','Converged Infrastructure','HCI','Smart Grid','XR','차세대 Healthcare Service','멀티 클라우드',
 'CCTV 개인영상 보호방안','12 Factors','C-V2X','Appliance Server','MPEG',
 
-/* 5주차 */
+// 5주차 
 '타당도','신뢰도','능동학습','opinion','회귀','주성분','다차원 척도','아웃소싱','sla','penalty','sip','nosql','증류','기술통계','추론통계','데이터 품질관리 지침','NLP','큐잉','군집분석','IT 투자','API Gateway','최단경로','국제 표준화 기구',
 'Efficient','지능 정보화 기본법','de facto standard','TensorFlow','스타스키마','ERD','관계형 데이터 모델','DataGrid','Graph Database','Back Tracking','분할과정복','Greedy','동적계획법','인공지능 데이터 평가','GAN','Vanishing Gradient Problem',
 'Data Warehouse','Data Lake','HOt Encoding','Softmax','AI 윤리','성능검증','머신러닝 파이프라인','Gradient Descent','정렬',
 
-/* ITPE 6 */
+// ITPE 6 
 '데이터베이스 트랜잭션', 'DaaS 인증','MC/DC','YOLO','Faster RCNN','랜섬웨어','표적형 랜섬웨어','XR','VPN','유전자 알고리즘','비대면 기술','PyTorch','마이데이터','한국판 AI 윤리기준','인공지능 규범 동향','바이오 인증','형상관리','마이데이터','성능 테스트','자기주권형 신원증명','Data Lake','Data Warehouse','IPv6','디지털 트윈','블록암호화','데이터 표준화','전송부호화','소프트웨어 아키텍처','ITIL','페어와이즈','MLOps','병렬 컴퓨팅','KANO Model',
 
-/* 탐구 학습 */
+// 탐구 학습 
 '클라우드 네이티브 애플리케이션','12 Factors','저전력',
 
-/* Mentor's Pick */
+// Mentor's Pick 
 'SD-WAN','ARIES','Join','MVCC',
 
-/* 암기 목록 */
+// 암기 목록 
 'ISO/IEC 20889', '식별자', 'ISMS-P','ISA/IEC 62443','GDPR','Privacy By Design',
 
-/* 라이지움 90회 */
+// 라이지움 90회 
 '초자동화','ASIL','디지털서비스 전문계약제도','ISO 25000','가설검정','DB 이상현상','Text Mining','Unix',
 
-/* ITPE 7회 */
+// ITPE 7회 
 '데이터 댐', 'Solid - Social Linked Data','Cloud 보안','OKR','KAMP','Computer Vision','6G','Digital Cartel','ISO 22301','회귀 분석','상관 분석','정규화','-21년 개정사항','감리 계획서, 보고서 작성 원칙','이동통신 역사','SA','NSA','6G 추진 전략','클라우드 포렌식','Activation Function','Transformer','망분리','망연계','Zero Pay','클라우드 네이티브 컴퓨팅','DevOps','인공지능 성능 지표','IoU','CBDC','Living Lab','SOS Lab','SW안전 확보 지침','SWEBOK','DMBOK','BABOK','백투백', 'Fuzz',
+*/
 ];
