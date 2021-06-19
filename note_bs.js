af1e8be4fd8134d0f6bb9c50476ca886e7808d65
Tigunews <gkr0325@hanmail.net>
2021-06-20 02:12:49 +0900

    202106200212

--- note_bs.js
+++ note_bs.js
@@ -785,24 +785,23 @@ var answer = answer.concat(
 // BSC
 '# 정의 : 재무중심 한계 / 재고내학 / 성과 관리 다중분석 기법 <br/>\
 - Balance Score Card <br/>\
-- 기존 재무 중심의 성과관리 한계를 보완하기 위해 재무, 고객, 내부 프로세스, 학습과 성장 관점을 관리한 성과 관리 다중분석 기법 <br/><br/>\
+- 재무적 성과지표 외 고객, 학습과 성장, 프로세스 고도화 관련 지표를 관리함으로써 기업의 중/단기 가치제고를 위한 균형적인 성과관리기법 <br/><br/>\
+# 수행 프로세스 <br/>\
+- 비전과 전략 구체화 : 비전 명확화, 전략위한 조직 내 컨센서스 도출 <br/>\
+- 의사소통 : 비전, 전략 명확한 인식, 보상 연결 <br/>\
+- 계획 및 목표 설정 : 이니셔티브 정렬, 자원 배분 연계 <br/>\
+- 전략적 피드백과 학습 : 제공 학습, 학습 촉진 <br/><br/>\
+# 구성요소 <br/>\
+- Prospective : 재무, 고객, 프로세스, 학습과 성장 <br/>\
+- CSF : 비전, 전략 성공 관리 요소 <br/>\
+- Strategy Map : BSC상 CSF간 인과관계, 조직 가치 창출 방법 <br/>\
+- KPI : CSF의 수준과 성공 여부 측정 및 고나리 방법 표현 지표 <br/>\
+- Target : 각 KPI 기간별 목표 수준 수치화 표현 <br/>\
+- Initiative : 기간별 KPI 목표 달성 위한 구체적인 활동 <br/><br/>\
 # 관점 (<font color = "red">재고내학</font>)<br/>\
-- 재무 : 주주배당이익/현금흐름, EVM, 회전율/ABM, VBM <br/>\
-- 고객 : 고객만족, 납기준수/만족도, 유지율/CRM <br/>\
-- 내부프로세스 : 품질, 생산효율/신제품출하율, 재고수준/SixSigma <br/>\
-- 학습과성장 : 신제품개발/직원만족도, 전략이행정도/지식경영 <br/><br/>\
-# 구축 프로세스 (BSC 전략 구체화) : 비전 및 전략 -> 전략맵 -> CSF -> KPI -> KPI 상세설계 <br/><br/>\
-# 특징 <br/>\
-- 목적 : 재무적 성장관계 파악(비즈니스, 프로세스, 고객 서비스, 제품 개선) <br/>\
-- 목표 : 기업의 행동과 프로세스 변화 <br/>\
-- 활동 : 비즈니스 거버넌스 <br/>\
-- 중점분야 : 비즈니스 분야 <br/>\
-- 활용사례 : 경영전략 수립, 주주대상 보고 <br/>\
-- 고려사항 : 기업내 전 영역 종합적 평가 필요 <br/><br/>\
-# 지표 <br/>\
-- CSF : 예)시장점유율 <br/>\
-- KPI : 선행지표, 예) 고객만족도, 배달시간 <br/>\
-- KGI : 후행지표 <br/><br/>\
+<img src = "./img/BSC.png" style = "max-width:100%; height:auto;"><br/><br/>\
+# 대상, KPI <br/>\
+<img src = "./img/BSC_KPI.png" style = "max-width:100%; height:auto;"><br/><br/>\
 * ITPE 합숙 124회 4일차 2교시 2번\
 ',
 
@@ -810,17 +809,11 @@ var answer = answer.concat(
 '# 정의 : IT Paradox / BSC 융합 / IT 성과 측정도구 <br/>\
 - IT생산성 paradox를 해결하기 위해 경영 BSC개념을 융합하여 개발한 IT 성과측정도구 <br/><br/>\
 # 관점 (<font color = "red">기사운미</font>) <br/>\
-- 기업공헌도 : ROI, TCO <br/>\
-- 사용자 : IT 서비스공급자, 사용자 파트너쉽, 사용자 만족, 고객 만족도, 납기 준수 <br/>\
-- 운영프로세스 : 프로세스 성숙도, 문제건수 <br/>\
-- 미래 지향 : 전문가 비율, 표준준수 여부 <br/><br/>\
-# 특징 <br/>\
-- 목적 : 내부 기술에 대한 고객 서비스 측면 성과 측정 <br/>\
-- 목표 : IT Value 전달 및 성과 달성 목적 제시 <br/>\
-- 활동 : IT 거버넌스 <br/>\
-- 중점분야 : 기술적 분야 <br/>\
-- 활용사례 : IT 부서평가, IT 투자 전개 참고 <br/>\
-- 고려사항 : IT 담당자, 주요관점별 실무지표 <br/><br/>\
+<img src = "./img/IT-BSC.png" style = "max-width:100%; height:auto;"><br/><br/>\
+# 비교 <br/>\
+<img src = "./img/BSC_IT-BSC.png" style = "max-width:100%; height:auto;"><br/><br/>\
+# Mapping <br/>\
+<img src = "./img/BSC_IT-BSC_Mapping.png" style = "max-width:100%; height:auto;"><br/><br/>\
 * ITPE 합숙 124회 4일차 2교시 2번\
 ',
 
