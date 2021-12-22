fccb7f3b82320722d37800e6eed43fbe4c49e9a0
Tigunews <gkr0325@hanmail.net>
2021-12-22 14:25:49 +0900

    Update note_toeic_A.js

--- note_toeic_A.js
+++ note_toeic_A.js
@@ -135,7 +135,7 @@ var answer = answer.concat(
 'I go out to see musicians perform live, at least once a year. The last time I went was last summer',
 'The price is quite reasonable, and you can love it',
 'So you can try to buy tickets',
-'You can buy local conert tickets online. I guess they are pretty expensive, because they tent to be about a hundred and ten tounsand won',
+'You can buy local conert tickets online. I guess they are pretty expensive, because they tend to be about a hundred and ten tounsand won',
 'It will last one hour',
 'Sonia Pinto will bel giving a lecture from eleven to noon ans also from three to four P.M.',
 'At eleven A.M., she\'ll be giving a lecture about small business adverising tips',
