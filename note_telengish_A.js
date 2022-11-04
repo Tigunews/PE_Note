81a8da1b31c804bbea0561fdf63f1255b4497445
Tigunews <gkr0325@hanmail.net>
2022-11-05 07:46:41 +0900

    202211050746

---
+++ note_telengish_A.js
@@ -0,0 +1,9 @@
+var answer = answer.concat(
+'I live close to my relatives.',
+'I see them every two to three months.',
+'I attended my cousin/'s wedding one month ago',
+'I have been playing the durms',
+'My family consists of four people.',
+'When I handling a naughty kid, I can pacify her with snack',
+'It was very tough. But I do that because we are famly.',
+);
---
+++ note_telenglish_A.js
@@ -0,0 +1,4 @@
+var answer = answer.concat(
+'I live close to my relatives.',
+'I see them every two to three months.',
+);
