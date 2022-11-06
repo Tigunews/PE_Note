c73549ad467a6401cd8345919776ca4bf4023c8c
Tigunews <gkr0325@hanmail.net>
2022-11-06 23:11:35 +0900

    202211061111

--- note_telenglish_A.js
+++ note_telenglish_A.js
@@ -1,4 +1,9 @@
 var answer = answer.concat(
 'I live close to my relatives.',
 'I see them every two to three months.',
+'get away',
+'on tap',
+'Because of COVID-19, We couldn\'t go on my honeymoon properly.',
+'I remember my trip Thailand on a shor-term misson',
+'If I had to choose just one of these, I would like to go to Jeju Island. Because there are various summer tourist attractions here.',
 );
