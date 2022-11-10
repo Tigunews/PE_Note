fd1aed305ab0e0f8f2125536325b6250f89784a1
Tigunews <gkr0325@hanmail.net>
2022-11-11 06:41:29 +0900

    202211110641

--- note_telenglish_A.js
+++ note_telenglish_A.js
@@ -18,7 +18,7 @@ var answer = answer.concat(
 'I didn\'t live in Jeju Island, I just went for sightseeing',
 'Halla Mountain is easy to climb because it has a gentle slope, but the course is very long',
 'I have never been to Canada or Mexico <br/>\
-I\'ve never set foot in Canada or Mexico\',
+I\'ve never set foot in Canada or Mexico',
 'My trip to Thailand is very secure',
 'I didn\'t visit Hawaii',
 );
